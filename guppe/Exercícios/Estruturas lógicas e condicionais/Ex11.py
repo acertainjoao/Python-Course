b633c4fce30af7e9377dcc28dddfc1dde90c7a24
acertainjoao <joaomiranda2211@gmail.com>
2022-02-07 14:20:41 +0000

    Inicialização

--- guppe/Exercícios/Estruturas lógicas e condicionais/Ex11.py
+++ guppe/Exercícios/Estruturas lógicas e condicionais/Ex11.py
@@ -4,8 +4,16 @@ exemplo, ao número 251 corresponderá o valor 8 (2+5+1). Se o número lido não
 com a mensagem 'Número inválido'.
 """
 
-numero = int(input('Insira um número inteiro maior que zero à sua escolha.'))
-res = 0
 
-if numero <= 0:
-    print('Número inválido.')
+numero = int(input('Insira um número inteiro maior que zero à sua escolha.'))
+
+soma = 0
+
+if numero > 0:
+    while numero != 0:
+        resto = numero % 10
+        numero = (numero - resto)//10
+        soma = soma + resto
+    print(soma)
+else:
+    print('Número inválido!')
